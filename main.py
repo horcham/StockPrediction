d4d8e802658888f7842bb7eff91d0846702012a1
ZHQ <690936541@qq.com>
2019-05-09 15:42:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import numpy as np
 import pandas as pd
-from StockData import Stock
-from utils import IndexCalculator
-from GlobalParams import *
+import zystock as zys
 
 
 if __name__ == '__main__':
-    stock = Stock('600019')
+    stock = zys.Stock('600019')
     print(stock.data)
-    indexcalc = IndexCalculator()
-    stockdata = indexcalc.PickIndexs(stock.data, PICKINDEXLIST)
+    indexcalc = zys.IndexCalculator()
+    stockdata = indexcalc.PickIndexs(stock.data, zys.PICKINDEXLIST)
     print(stockdata)
